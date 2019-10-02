7d4d225e3c77e7b39484a0249c340e82c6912212
Andrey Vershinin <a.vershinin@reg.ru>
2019-10-02 23:42:11 +0300

    implement solution for 2-module 2-task

--- 2-module/2-task/LineSplitStream.js
+++ 2-module/2-task/LineSplitStream.js
@@ -1,15 +1,36 @@
+const {StringDecoder} = require('string_decoder');
 const stream = require('stream');
-const os = require('os');
+const {EOL} = require('os');
 
 class LineSplitStream extends stream.Transform {
-  constructor(options) {
+  constructor(options = {}) {
     super(options);
+    this._encoding = options.encoding || 'utf-8';
+    this._decoder = new StringDecoder(this._encoding);
+    this._str = '';
   }
 
   _transform(chunk, encoding, callback) {
+    const chunkStr = this._decoder.write(chunk);
+    const currentStr = `${this._str}${chunkStr}`;
+
+    if (currentStr.includes(EOL)) {
+      const lines = currentStr.split(EOL);
+
+      lines
+          .slice(0, -1)
+          .forEach((line) => this.push(line));
+
+      this._str = lines.slice(-1)[0];
+    } else {
+      this._str = currentStr;
+    }
+
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this._str);
   }
 }
 
