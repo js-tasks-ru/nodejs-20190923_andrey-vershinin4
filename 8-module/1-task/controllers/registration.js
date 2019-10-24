048141d1e4849a6e226d05921360fb877428c629
Andrey Vershinin <a.vershinin@reg.ru>
2019-10-24 09:05:46 +0300

    solve 8-module/1-task

--- 8-module/1-task/controllers/registration.js
+++ 8-module/1-task/controllers/registration.js
@@ -3,7 +3,49 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+  const userData = ctx.request.body;
+
+  // Проверяем, есть ли такой юзер
+  const user = await User.findOne({email: userData.email});
+
+  if (user) {
+    return ctx.throw(400, {errors: {email: 'Такой email уже существует'}});
+  }
+
+  // Новый юзер
+  const verificationToken = uuid();
+
+  const newUser = new User({...userData, verificationToken});
+  await newUser.setPassword(userData.password);
+  await newUser.save();
+
+  await sendMail({
+    template: 'confirmation',
+    locals: {token: verificationToken},
+    to: userData.email,
+    subject: 'Подтвердите почту',
+  });
+
+  ctx.body = {status: 'ok'};
 };
 
 module.exports.confirm = async (ctx, next) => {
+  const {verificationToken} = ctx.request.body;
+
+  if (!verificationToken) {
+    return ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+
+  const user = await User.findOne({verificationToken});
+
+  if (!user) {
+    return ctx.throw(400, 'Ссылка подтверждения недействительна или устарела');
+  }
+
+  user.verificationToken = undefined; // ???
+  await user.save();
+
+  const token = await ctx.login(user);
+
+  ctx.body = {token};
 };
