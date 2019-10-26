2aa0ba11ce761f550824efbc81586b5492641600
Andrey Vershinin <adw006@ya.ru>
2019-10-26 22:53:28 +0300

    clean up the code

--- 9-module/1-task/socket.js
+++ 9-module/1-task/socket.js
@@ -9,16 +9,14 @@ function socket(server) {
   // Check authentication before connect
   io.use(async function(socket, next) {
     // check token
-    const query = socket.handshake.query;
-    if (!query.token) {
-      // console.error('Error: anonymous sessions are not allowed');
+    const {token} = socket.handshake.query;
+    if (!token) {
       return next(new Error('anonymous sessions are not allowed'));
     }
 
     // check if there's a session with such token
-    const session = await Session.findOne({token: query.token}).populate('user');
+    const session = await Session.findOne({token}).populate('user');
     if (!session) {
-      // console.error('Error: wrong or expired session token');
       return next(new Error('wrong or expired session token'));
     }
 
@@ -28,11 +26,7 @@ function socket(server) {
 
 
   io.on('connection', async function(socket) {
-    // console.log('User connected on WS');
-
     socket.on('message', async (msg) => {
-      // console.info('Message:', msg);
-
       await Message.create({
         date: new Date(),
         text: msg,
